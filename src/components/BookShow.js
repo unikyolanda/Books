c33e065f539eb50a40d26bbc275142f6ea16d08e
unikyolanda <yolandastargirl@gmail.com>
2023-09-15 16:58:54 +0800

    Create a React app and publish it to GitHub Pages

---
+++ src/components/BookShow.js
@@ -0,0 +1,43 @@
+import { useContext, useState } from "react";
+import BooksContext from '../context/books';
+import BookEdit from './BookEdit';
+
+function BookShow({ book }) {
+    const [ showEdit, setShowEdit ] = useState(false);
+    const { deleteBookById } = useContext(BooksContext);
+
+    const handleDeleteClick = () => {
+        deleteBookById(book.id);
+    };
+
+    const hadleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+    };
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit book={book} onSubmit={handleSubmit}/>;
+    }
+
+    return <div className="book-show">
+        <img 
+            alt="books"
+            src={`https://picsum.photos/seed/${book.id}/300/200`}
+        />
+        <div>{content}</div>
+        <div className="actions">
+            <button className="edit" onClick={hadleEditClick}>
+                Edit
+            </button>
+            <button className="delete" onClick={handleDeleteClick}>
+                Delete
+            </button>
+        </div>
+        </div>
+}
+
+export default BookShow;
